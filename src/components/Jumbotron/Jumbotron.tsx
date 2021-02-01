93fdad451d69d7a4543e84e527364ff8367a6605
Olga Mykhaylovska <olga.mykhaylovska@gmail.com>
2021-02-01 11:44:36 +0200

    auto next in 10 sec

--- src/components/Jumbotron/Jumbotron.tsx
+++ src/components/Jumbotron/Jumbotron.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ImageData } from '../../data'
 import Actions from '../Actions'
 import Image from '../Image'
+
+const NEXT_TIMEOUT = 10 * 1000 // 10 sec
 
 type JumbotronProps = {
     images: ImageData[]
@@ -14,6 +16,13 @@ export const Jumbotron: React.FC<JumbotronProps> = ({ images }) => {
         setIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : images.length - 1))
     const handleNext = () =>
         setIndex((prevIndex) => (prevIndex < images.length - 1 ? prevIndex + 1 : 0))
+
+    useEffect(() => {
+        const timeoutId = images.length > 1 ? setTimeout(handleNext, NEXT_TIMEOUT) : undefined
+
+        return () => timeoutId && clearTimeout(timeoutId)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [images, index])
 
     if (images.length === 0) return <h3>No images</h3>
     if (images.length === 1) return <Image {...images[0]} />
