e364bafa2492054dea5e7f5a1f56daa2120e9043
Olga Mykhaylovska <olga.mykhaylovska@gmail.com>
2021-02-01 09:58:51 +0200

    image data

---
+++ src/data/index.ts
@@ -0,0 +1,11 @@
+export type ImageData = {
+    imageUrl: string
+    description: string
+}
+
+export const Images: ImageData[] = ['First', 'Second', 'Third', 'Fourth', 'Fifth'].map(
+    (description, idx) => ({
+        imageUrl: `https://picsum.photos/seed/${idx + 1}00/300`,
+        description: `${description} image`,
+    })
+)
