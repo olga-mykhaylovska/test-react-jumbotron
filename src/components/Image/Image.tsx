8996b0b94975e8a52f2c0af8bb66fb808441121d
Olga Mykhaylovska <olga.mykhaylovska@gmail.com>
2021-02-01 11:53:21 +0200

    better image design

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -2,5 +2,12 @@ import React from 'react'
 import { ImageData } from '../../data'
 
 export const Image: React.FC<ImageData> = ({ imageUrl, description }) => {
-    return <img src={imageUrl} alt={description} />
+    return (
+        <div className="card">
+            <img className="card-img-top" src={imageUrl} alt={description} />
+            <div className="card-body">
+                <p className="card-text">{description}</p>
+            </div>
+        </div>
+    )
 }
